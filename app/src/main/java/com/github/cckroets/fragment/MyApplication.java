879e1f64f0a659e910548e5e763a82b72d90faa9
ckroetsc <ckroetsch@linkedin.com>
2020-08-27 17:57:05 -0700

    Add documentation about behavior

--- app/src/main/java/com/github/cckroets/fragment/MyApplication.java
+++ app/src/main/java/com/github/cckroets/fragment/MyApplication.java
@@ -4,6 +4,31 @@ import android.app.Application;
 
 import androidx.fragment.app.FragmentManager;
 
+/**
+ * When the new State Manager behavior is enabled, the order of fragment lifecycle events
+ * as seen by FragmentLifecycleCallbacks is different. Specifically the order of a parent
+ * Fragment's onResume() call is observed after the child's onResume() call.
+ *
+ * Below are the logs from {@link LoggingCallbacks} with both behaviors:
+ *
+ * enableNewStateManager(true):                 enableNewStateManager(false):
+ *
+ * onFragmentPreAttached: ParentFragment        onFragmentPreAttached: ParentFragment
+ * onFragmentCreated: ParentFragment            onFragmentCreated: ParentFragment
+ * onFragmentViewCreated: ParentFragment        onFragmentViewCreated: ParentFragment
+ * onFragmentStarted: ParentFragment            onFragmentStarted: ParentFragment
+ * onFragmentPreAttached: ChildFragment{1}      onFragmentResumed: ParentFragment
+ * onFragmentCreated: ChildFragment{1}          onFragmentPreAttached: ChildFragment{1}
+ * onFragmentViewCreated: ChildFragment{1}      onFragmentCreated: ChildFragment{1}
+ * onFragmentStarted: ChildFragment{1}          onFragmentPreAttached: ChildFragment{2}
+ * onFragmentPreAttached: ChildFragment{2}      onFragmentCreated: ChildFragment{2}
+ * onFragmentCreated: ChildFragment{2}          onFragmentViewCreated: ChildFragment{1}
+ * onFragmentViewCreated: ChildFragment{2}      onFragmentStarted: ChildFragment{1}
+ * onFragmentStarted: ChildFragment{2}          onFragmentResumed: ChildFragment{1}
+ * onFragmentResumed: ChildFragment{1}          onFragmentViewCreated: ChildFragment{2}
+ * onFragmentResumed: ChildFragment{2}          onFragmentStarted: ChildFragment{2}
+ * onFragmentResumed: ParentFragment            onFragmentResumed: ChildFragment{2}
+ */
 public class MyApplication extends Application {
 
     // Logged from FragmentLifecycleCallbacks
@@ -14,6 +39,7 @@ public class MyApplication extends Application {
 
     @Override
     public void onCreate() {
+        // Toggle this for different behavior
         FragmentManager.enableNewStateManager(true);
         super.onCreate();
     }
